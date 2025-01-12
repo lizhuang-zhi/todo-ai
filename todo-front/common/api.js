1070f810cf9b40c9e03f584cb0f85de5a8d494d2
Mr.KLeo <59167582+lizhuang-zhi@users.noreply.github.com>
2025-01-12 16:46:56 +0800

    update

--- todo-front/common/api.js
+++ todo-front/common/api.js
@@ -1,2 +1,5 @@
-export const ApiHost = "http://localhost:18888"
+// export const ApiHost = "http://127.0.0.1:18888"
+export const ApiHost = "http://192.168.1.3:18888"  // Mac的IP地址, 用于同一wifi下的手机访问
 export const AuthKey = "112233mrkleo"
+
+export const UserID = 2  // 用于切换用户
